d8d3a194ae61500838c5a1d3a72710d0ff9e1682
soungson <99154843+soungson@users.noreply.github.com>
2022-03-20 22:44:40 +0000

    Creation d'une classe pour le calcul des rubriques des salaires

---
+++ rubriques_salaire.py
@@ -0,0 +1,95 @@
+class Calcul_rubriques_salaire:
+  
+  def __init__(self, info_matricule, info_nom, info_prenom, info_telephone, info_email, info_dateNaissance, info_dateEmbaucge, info_datePaie, info_salaireBase, info_indemniteLogement, info_indemniteFonction, info_indemniteTransport, info_indemniteSujetion, info_indemniteTechnicite, info_indemniteCaisse, info_indemniteAutres, info_autresPrimes, info_avantagesNatureImmobilisation, info_avantagesNatureEmploye, info_allocationFamilliale, info_montantHeuresSupplementaires, info_avanceAcompte, info_oppositionSaisieArret, info_autresRetenues, info_chargeFamilial, info_categorie, info_institutionBancaire, info_numeroCompteBancaire, info_service, info_fonction, info_numeroCNSS, info_ifuEmployeur):
+    self.info_matricule = info_matricule
+    self.info_nom = info_nom
+    self.info_prenom = info_prenom
+    self.info_telephone = info_telephone
+    self.info_email = info_email
+    self.info_dateNaissance = info_dateNaissance
+    self.info_dateEmbaucge = info_dateEmbaucge
+    self.info_datePaie = info_datePaie
+    self.info_salaireBase = info_salaireBase
+    self.info_indemniteLogement = info_indemniteLogement
+    self.info_indemniteFonction = info_indemniteFonction
+    self.info_indemniteTransport = info_indemniteTransport
+    self.info_indemniteSujetion = info_indemniteSujetion
+    self.info_indemniteTechnicite = info_indemniteTechnicite
+    self.info_indemniteCaisse = info_indemniteCaisse
+    self.info_indemniteAutres = info_indemniteAutres
+    self.info_autresPrimes = info_autresPrimes
+    self.info_avantagesNatureImmobilisation = info_avantagesNatureImmobilisation
+    self.info_avantagesNatureEmploye = info_avantagesNatureEmploye
+    self.info_allocationFamilliale = info_allocationFamilliale
+    self.info_montantHeuresSupplementaires = info_montantHeuresSupplementaires
+    self.info_avanceAcompte = info_avanceAcompte
+    self.info_oppositionSaisieArret = info_oppositionSaisieArret
+    self.info_autresRetenues = info_autresRetenues
+    self.info_chargeFamilial = info_chargeFamilial
+    self.info_categorie = info_categorie
+    self.info_institutionBancaire = info_institutionBancaire
+    self.info_numeroCompteBancaire = info_numeroCompteBancaire
+    self.info_service = info_service
+    self.info_fonction = info_fonction
+    self.info_numeroCNSS = info_numeroCNSS
+    self.info_ifuEmployeur = info_ifuEmployeur
+  
+  def prime_anciennete():
+    pass
+
+  def salaire_brut_en_numeraire():
+    pass
+
+  def taux_avantage_en_nature():
+    pass
+
+  def salaire_brut_total():
+    pass
+
+  def CNSS_Employe():
+    pass
+
+  def salaire_de_base_brut_imposable():
+    pass
+
+  def exoneration_logement():
+    pass
+
+  def exoneration_fonction():
+    pass
+
+  def exoneration_transport():
+    pass
+
+  def exoneration_total():
+    pass
+
+  def abattement_forfaitaire():
+    pass
+
+  def salaire_base_imposable():
+    pass
+
+  def IUTS_brut():
+    pass
+
+  def abattement_sur_charges_familliales():
+    pass
+
+  def IUTS_net():
+    pass
+
+  def CNSS_Employeur():
+    pass
+
+  def TPA():
+    pass
+
+  def total_retenues():
+    pass
+
+  def salaire_a_payer():
+    pass
+
+  def charge_salariale_totale():
+    pass
